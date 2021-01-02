9d4c0eb6cfcbd624a4e6fcc3be7cad783e043f84
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-01-02 22:03:35 +0900

    build unit test

--- hi-nest/src/movies/movies.service.spec.ts
+++ hi-nest/src/movies/movies.service.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -8,11 +10,84 @@ describe('MoviesService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
     }).compile();
-
     service = module.get<MoviesService>(MoviesService);
+
+    service.create({
+      title: "Test Movie",
+      genres: ["test"],
+      year: 2020,
+    });
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe("getAll", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne", () => {
+    it("should return a movie ", () => {
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+
+    it("should throw a NotFoundException", () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe("deleteOne", () => {
+    it("deletes a movie", () => {
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it("should throw a NotFoundException", () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe("create", () => {
+    it("should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2020,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe("update", () => {
+    it("should update a movie", () => {
+      service.update(1, { title: "update test" });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual("update test");
+    });
+  });
+
+  it("should throw a NotFoundException", () => {
+    try {
+      service.update(999, { title: "update test" });
+    } catch (e) {
+      expect(e).toBeInstanceOf(NotFoundException);
+    }
   });
 });
