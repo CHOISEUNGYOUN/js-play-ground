fdd4314a9854174ede15c161c1498b01338b9b5e
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-01-08 23:37:19 +0900

    add useFadeIn

--- nooks/useFadeIn/useFadeIn.js
+++ nooks/useFadeIn/useFadeIn.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-
-const useFadeIn = (duration = 1, delay = 0) => {
+export const useFadeIn = (duration = 1, delay = 0) => {
   const element = useRef();
   useEffect(() => {
     if (typeof duration !== "number" || typeof delay !== "number") {
@@ -14,16 +12,3 @@ const useFadeIn = (duration = 1, delay = 0) => {
   }, []);
   return { ref: element, style: { opacity: 0 } };
 };
-
-function App() {
-  const fadeInH1 = useFadeIn(1, 2);
-  const fadeInP = useFadeIn(5, 10);
-  return (
-    <div className="App">
-      <h1 {...fadeInH1}>hello</h1>
-      <p {...fadeInP}>blablablablablablablabla</p>
-    </div>
-  );
-}
-
-export default App;
